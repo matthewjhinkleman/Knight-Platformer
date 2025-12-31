e29766c524f9db590725cbb7d737b874ddad3866
Matthew Hinkleman <matthewjhinkleman@gmail.com>
2025-12-31 13:40:24 -0800

    Added assets, finished level 2, and created a title screen

---
+++ Game/scripts/title_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var first_level: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Music.play_music(preload("res://assets/music/flowerbed_fields.ogg"))
+
+func _process(_delta):
+	# Detect input to start game
+	if Input.is_action_just_pressed("ui_accept"):  # map "START" button
+		start_game()
+
+func start_game():
+	# Load the first level
+	get_tree().change_scene_to_packed(first_level)
